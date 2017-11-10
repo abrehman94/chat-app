e5cb4e4e6ab13a84eb1b923df6dbdb6c1f180d96
abrehman94 <abrehman1994@gmail.com>
2017-11-11 00:49:15 +0500

    'add'

--- server/server.js
+++ server/server.js
@@ -3,15 +3,28 @@ const publicPath = path.join(__dirname+"/../public");
 const express = require("express");
 const port = process.env.PORT || 3000;
 const hbs = require ("hbs");
+const socketIO = require("socket.io");
+const http = require("http");
 
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on("connection",(socket)=>{
+    console.log("connected");
+    socket.on("disconnect", ()=>{
+        console.log("client disconnected");
+    });
+});
+
+
 
 // app.set('views', (publicPath+"/views"));
 // app.set('view engine', 'hbs');
 // app.get("/", function(req,res){
 //     res.render("index.hbs");
 // });
-
-app.listen(port, () => console.log(`Listening on ${port} `));
+console.log("hello");
+server.listen(port, () => console.log(`Listening on ${port} `));
